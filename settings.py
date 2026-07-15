526b905e7296052b1a1a77d2bdcbf7741b9ef1e3
jeb <jeb774455@gmail.com>
2026-07-15 16:13:23 +0200

    Fix formatting and update variable name for tile images

--- settings.py
+++ settings.py
@@ -1,7 +1,9 @@
 import os
-#Global settings
+
+# Global settings
 RES_X = 800
 RES_Y = 600
 FPS = 60
-#пути к картинкам. Пока как предложение, нигде не используется
-PATH_tile_images =os.path.join('Images','Tiles')
+
+# Path to tile images
+PATH_TILE_IMAGES = os.path.join('Images', 'Tiles')
